f4830d7155330db5ecef37e02dc422cdad0575d0
AC-AUDIOCRAFTER <christopherjaramillo623@gmail.com>
2026-04-27 14:45:57 -0700

    Update audiocrafter.js

--- api/audiocrafter.js
+++ api/audiocrafter.js
@@ -1,4 +1,21 @@
+import fs from "fs";
+import path from "path";
+
+const VALID_KEY = "ACMelody2024secretkey";
+
 export default function handler(req, res) {
+  const key = req.query.key;
+
+  // :x: wrong or missing key
+  if (key !== VALID_KEY) {
+    res.status(403).setHeader("Content-Type", "text/plain");
+    return res.send("Forbidden");
+  }
+
+  const filePath = path.join(process.cwd(), "private", "ac.lua");
+  const lua = fs.readFileSync(filePath, "utf8");
+
+  res.status(200);
   res.setHeader("Content-Type", "text/plain");
-  return res.send('print("TEST OK")');
+  return res.send(lua);
 }
